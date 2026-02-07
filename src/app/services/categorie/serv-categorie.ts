84c56415bcd6166c29dcf148aa05abc95109694e
segundo77 <segundocama7@gmail.com>
2026-02-06 19:08:53 -0500

    Cambios Categorie

--- src/app/services/categorie/serv-categorie.ts
+++ src/app/services/categorie/serv-categorie.ts
@@ -1,14 +1,15 @@
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpParams } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { map, Observable } from 'rxjs';
 import { Categorie } from '../../models/Categorie';
 
+/*
 @Injectable({
   providedIn: 'root',
 })
 export class ServCategorie {
 
-  private apiUrl = "http://localhost:3000/categories";
+  private apiUrl = "http://localhost:5005/api/Categories";
 
   constructor(private http: HttpClient) {}
 
@@ -84,5 +85,52 @@ export class ServCategorie {
 
   private getCurrentDate(): string {
     return new Date().toISOString().split('T')[0];
+  }
+}
+  */
+
+@Injectable({
+  providedIn: 'root'
+})
+export class ServCategorie {
+  // Cambia esta URL a la de tu API local
+  private apiUrl = 'http://localhost:5005/api/Categories';
+
+  constructor(private http: HttpClient) { }
+
+  getCategories(): Observable<Categorie[]> {
+    return this.http.get<Categorie[]>(this.apiUrl);
+  }
+
+  getCategory(id: number): Observable<Categorie> {
+    return this.http.get<Categorie>(`${this.apiUrl}/${id}`);
+  }
+
+  addCategorie(categorie: Categorie): Observable<Categorie> {
+    // IMPORTANTE: No envíes el ID si es auto-increment en la API
+    const { id, ...categorieWithoutId } = categorie;
+    return this.http.post<Categorie>(this.apiUrl, categorieWithoutId);
+  }
+
+  updateCategorie(categorie: Categorie): Observable<any> {
+    return this.http.put(`${this.apiUrl}/${categorie.id}`, categorie);
+  }
+
+  deleteCategorie(id: number): Observable<any> {
+    return this.http.delete(`${this.apiUrl}/${id}`);
+  }
+
+  searchCategories(search: string): Observable<Categorie[]> {
+    const params = new HttpParams().set('search', search);
+    return this.http.get<Categorie[]>(`${this.apiUrl}/search`, { params });
+  }
+
+  // Métodos adicionales si los implementaste en el controlador
+  deactivateCategory(id: number): Observable<any> {
+    return this.http.put(`${this.apiUrl}/deactivate/${id}`, {});
+  }
+
+  activateCategory(id: number): Observable<any> {
+    return this.http.put(`${this.apiUrl}/activate/${id}`, {});
   }
 }
